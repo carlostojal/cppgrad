899d26481b093cc51778d37f2c8f546b50db340a
Carlos Tojal <carlos.c.tojal@gmail.com>
2026-07-04 18:07:43 +0100

    create some status codes

---
+++ include/common.h
@@ -0,0 +1,16 @@
+#ifndef __COMMON_H__
+#define __COMMON_H__
+
+#include <cstdint>
+
+namespace cppgrad
+{
+
+	enum class status_e
+	{
+		numerical_err,
+		linalg_err
+	};
+}
+
+#endif // __COMMON_H__
