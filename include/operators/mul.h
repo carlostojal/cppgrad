5f4b9a8ab10a5267f6cee3f2f1c239663a5f9d73
Carlos Tojal <carlos.c.tojal@gmail.com>
2026-07-28 22:29:18 +0100

    plan multiplication operator

---
+++ include/operators/mul.h
@@ -0,0 +1,32 @@
+#ifndef __MUL_H__
+#define __MUL_H__
+
+#include "operators/op.h"
+
+namespace cppgrad::operators
+{
+	/**
+	 * @class mul
+	 * @brief Multiplication (product) operator.
+	 */
+	template <typename ValueT>
+	class mul : public op
+	{
+	public:
+		/**
+		 * @brief Multiplication computation.
+		 * @param op1 Left-hand operand.
+		 * @param op2 Right-hand operand.
+		 * @param out Result value.
+		 */
+		void forward(const linalg::tensor<ValueT>& op1, const linalg::tensor<ValueT>& op2, linalg::tensor<ValueT>& out) noexcept override;
+
+		/**
+		 * @brief Multiplication gradient.
+		 * @param out Gradient value.
+		 */
+		void grad(linalg::tensor<ValueT>& out) noexcept override;
+	};
+}
+
+#endif // __MUL_H__
