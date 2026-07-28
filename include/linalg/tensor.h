bc291b38bd99520c7f32403433d2940e4e4ee823
Carlos Tojal <carlos.c.tojal@gmail.com>
2026-07-28 22:27:19 +0100

    add element get and set methods to tensor class

--- include/linalg/tensor.h
+++ include/linalg/tensor.h
@@ -1,6 +1,11 @@
 #ifndef __TENSOR_H__
 #define __TENSOR_H__
 
+#include "common.h"
+
+#include <vector>
+#include <cstdlib>
+
 namespace cppgrad::linalg
 {
 	/**
@@ -11,7 +16,49 @@ namespace cppgrad::linalg
 	template <typename ScalarT>
 	class tensor
 	{
+	public:
+		/**
+		 * @brief Constructor.
+		 * @param dims Tensor dimensions.
+		 */
+		tensor(const std::vector<std::size_t>& dims) noexcept;
 
+		/**
+		 * @brief Destructor.
+		 */
+		~tensor(void) noexcept;
+		
+		/**
+		 * @brief Get an element of the tensor.
+		 * @param idx Index.
+		 * @param out Value at index.
+		 * @return Status code.
+		 */
+		status_e get_elem(const std::vector<std::size_t>& idx, ScalarT& out) const noexcept;
+
+		/**
+		 * @brief Set an element of the tensor.
+		 * @param idx Index.
+		 * @param out Value at index.
+		 * @return Status code.
+		 */
+		status_e set_elem(const std::vector<std::size_t>& idx, ScalarT value) noexcept;
+
+	private:
+
+		/**
+		 * @brief Get the flatenned tensor index for a given n-dimensional index.
+		 * @param idx n-dimensional index.
+		 * @param flat_idx Flatenned index.
+		 * @return Status code.
+		 */
+		status_e get_flat_idx(const std::vector<std::size_t>& idx, std::size_t& flat_idx) const noexcept;
+
+		/// Tensor dimensions.
+		std::vector<std::size_t> dims_;
+
+		/// Tensor values.
+		ScalarT* values_ = nullptr;
 	};
 }
 
