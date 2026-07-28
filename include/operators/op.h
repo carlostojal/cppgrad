c78bd4ca3fb7dfe182c735d76cbfff86f7a450e3
Carlos Tojal <carlos.c.tojal@gmail.com>
2026-07-28 22:28:55 +0100

    return operator gradients by reference

--- include/operators/op.h
+++ include/operators/op.h
@@ -10,6 +10,7 @@ namespace cppgrad::operators
 	 * @class op
 	 * @brief Abstract operator.
 	 */
+	template <typename ScalarT>
 	class op : public node
 	{
 	public:
@@ -19,13 +20,13 @@ namespace cppgrad::operators
 		 * @param op2 Right-hand operand.
 		 * @param out Result value.
 		 */
-		virtual void forward(const linalg::tensor& op1, const linalg::tensor& op2, linalg::tensor& out) noexcept = 0;
+		virtual void forward(const linalg::tensor<ScalarT>& op1, const linalg::tensor<ScalarT>& op2, linalg::tensor<ScalarT>& out) noexcept = 0;
 
 		/**
 		 * @brief Operator gradient.
 		 * @param out Gradient value.
 		 */
-		virtual linalg::tensor grad(linalg::tensor& out) noexcept = 0;
+		virtual void grad(linalg::tensor<ScalarT>& out) noexcept = 0;
 	};
 }
 
